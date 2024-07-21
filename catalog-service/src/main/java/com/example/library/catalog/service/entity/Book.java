e161e9e654edba143e5760e4319b8277fad81407
ankit jibhakate <ankitubj@gmail.com>
2024-07-21 14:37:49 +0530

    update properties file

---
+++ catalog-service/src/main/java/com/example/library/catalog/service/entity/Book.java
@@ -0,0 +1,87 @@
+package com.example.library.entity;
+
+import com.example.library.catalog.service.entity.Author;
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    //    private String author;
+    private Double price;
+    private Integer yearPublished;
+
+    private Boolean isDeleted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "author_id", referencedColumnName = "id")
+    private Author author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publisher_id", referencedColumnName = "id")
+    private com.example.library.entity.Publisher publisher;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setYearPublished(Integer yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+//    public String getAuthor() {
+//        return author;
+//    }
+
+//    public void setAuthor(String author) {
+//        this.author = author;
+}
+
